220097af76ccc804dad55d42909164e4da39cf77
Francesco Saccone <fs@frasac.com>
2025-12-13 17:27:59 +0100

    feat: implement basic argument parsing

--- md.c
+++ md.c
@@ -1,8 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arg.h"
+
+void usage(void);
+
+void
+usage(void)
+{
+	fprintf(stderr, "Usage: %s [file]\n", argv0);
+	exit(EXIT_FAILURE);
+}
+
 int
 main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	ARGBEGIN {
+	default:
+		usage();
+	} ARGEND
 
 	return 0;
 }
