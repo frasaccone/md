7a76cb06e6b82abf15560d945b40f497dcb934fe
Francesco Saccone <francesco@francescosaccone.com>
2025-12-24 17:52:28 +0100

    feat(parse.c): define lastopenchild

--- parse.c
+++ parse.c
@@ -1,5 +1,24 @@
 #include "parse.h"
 
+#include <stdlib.h>
+
+/* It returns the last open child of block b. If b has no open children, NULL
+   is returned instead. */
+struct block *lastopenchild(struct block *b);
+
+struct block *
+lastopenchild(struct block *b)
+{
+	struct block *c, *lastopen = NULL;
+
+	for (c = b->children; c && c->sibling; c = c->sibling) {
+		if (!c->closed)
+			lastopen = c;
+	}
+
+	return lastopen;
+}
+
 void
 parseline(struct block *document, char *line)
 {
