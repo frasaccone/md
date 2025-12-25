f8d6b380df304faaca05a5c28685d042083f67b4
Francesco Saccone <francesco@francescosaccone.com>
2025-12-25 18:31:51 +0100

    chore: do not say that perror is called in comments

--- buffer.h
+++ buffer.h
@@ -7,8 +7,8 @@
    of length newlen at its end. Lengths are to be calculated without the NULL
    character.
 
-   In case of error, perror is called and NULL is returned; otherwise, the
-   reallocated buffer is returned. */
+   In case of error, NULL is returned; otherwise, the reallocated buffer is
+   returned. */
 char *addtobuffer(char *buf, size_t buflen, char *new, size_t newlen);
 
 #endif
--- mul.h
+++ mul.h
@@ -34,8 +34,8 @@ struct mulnode {
 	struct mulnode *children;
 };
 
-/* It allocates and returns a document node. In case of error, perror is called
-   and NULL is returned instead. */
+/* It allocates and returns a document node. In case of error, NULL is returned
+   instead. */
 struct mulnode *muldocument(void);
 
 /* It updates the document node after parsing the given buf buffer of length
